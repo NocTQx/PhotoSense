8cabac6d07d08aed92e8202cd7f243a2f8fddec4
NocTQx <t1tovalyuba@yandex.ru>
2026-06-11 21:26:41 +0300

    init commit

---
+++ src/main/java/org/photosense/PhotoSenseApp.java
@@ -0,0 +1,12 @@
+package org.photosense;
+
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PhotoSenseApp {
+    public static void main(String[] args) {
+        SpringApplication.run(PhotoSenseApp.class, args);
+    }
+}
